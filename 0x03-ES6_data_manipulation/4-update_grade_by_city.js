24514f6736b13d0cca3af8911954ac974868c376
root <mathewkoech>
2024-07-30 14:27:19 +0000

    function updateStudentGradeByCity that returns an array of students for a specific city with their new grade

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,18 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || typeof city !== 'string' || !Array.isArray(newGrades)) {
+    console.error('Invalid input types');
+    return [];
+  }
+
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
+
+export default updateStudentGradeByCity;
