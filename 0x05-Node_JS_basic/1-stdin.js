d4908140641dcb56e91d4bf52f356fba3cefccc3
mathewkoech <mathewkoech55@gmail.com>
2024-09-16 22:49:01 +0300

    add stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+// A programme that will be executed through command line
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
+
