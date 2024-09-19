e9e039d046d793ec238dd745babb1c59c5aaa5c8
mathewkoech <mathewkoech55@gmail.com>
2024-09-19 22:49:58 +0300

    Post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 7865;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Home endpoint
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to the payment system');
+})
+
+// ID endpoint
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+
+// Payments Endpoint
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
+});
+
+// Login endpoint
+app.post('/login', (req, res) => {
+  const username = req.body.userName || 'Unknown';
+  res.send(`Welcome ${username}`);
+});
+
+app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`);
+})
+
+module.exports = app;
