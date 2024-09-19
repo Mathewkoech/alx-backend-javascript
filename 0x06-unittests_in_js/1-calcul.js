4d7774286f0496f57b43d7710dfe202301f38cfd
mathewkoech <mathewkoech55@gmail.com>
2024-09-19 13:49:57 +0300

    add calculate for several types

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,23 +1,19 @@
-function calculateNumber (type, a, b) {
+function calculateNumber(type, a, b) {
     const roundedA = Math.round(a);
     const roundedB = Math.round(b);
-
-    //operations based on type
-
-    switch (type) {
-        case 'SUM':
-            return roundedA + roundedB;
-
-        case 'SUBTRACT':
-            return roundedA - roundedB;
-
-        case 'DIVIDE':
-            if (roundedB === 0) {
-                return 'Error';
-            }
-            return roundedA / roundedB;
-        
-
-    }
     
+    if (type === 'SUM') {
+        return roundedA + roundedB;
+    } else if (type === 'SUBTRACT') {
+        return roundedA - roundedB;
+    } else if (type === 'DIVIDE') {
+        if (roundedB === 0) {
+            return "Error"
+        }
+        return roundedA / roundedB;
+    } else {
+        throw new Error('Invalid type');
+    }
 }
+
+module.exports = calculateNumber;
